998195b3f02034c41e097132b1c5fc436b325c91
dmitry.e.sorokin <dmitry.e.sorokin@localhost>
2007-09-24 15:24:00 +0000

    Initial urls.py

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^registration/login', 'django.contrib.auth.views.login'),
+    (r'^registration/logout', 'django.contrib.auth.views.logout'),
+    #(r'^registration/register/$', 'serpantin.apps.registration.views.register'),
+    (r'^registration/password_reset/$', 'django.views.registration.passwords.password_reset'),
+    (r'^registration/password_reset/done/$', 'django.views.registration.passwords.password_reset_done'),
+    (r'^registration/password_change/$', 'django.views.registration.passwords.password_change'),
+    (r'^registration/password_change/done/$', 'django.views.registration.passwords.password_change_done'),
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'registration/index.html'}),
+
+    (r'^accounts/profile/$', 'django.views.generic.simple.direct_to_template', {'template': 'base_dojo.html'}),
+    (r'^accounts/login', 'django.views.auth.login.login'),
+)
