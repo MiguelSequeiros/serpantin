da1cc05bbf8035dca575d40788772b8564984cf8
subzero.debian <subzero.debian@localhost>
2007-10-24 15:53:00 +0000

    Query test

--- apps/test/views.py
+++ apps/test/views.py
@@ -10,17 +10,27 @@ class JsonResponse(HttpResponse):
         self["Content-Type"] = "text/javascript"
 
     def serialize(self):
-        return(simplejson.dumps(self.original_obj))
+        return('/*' + simplejson.dumps(self.original_obj) + '*/')
 
 def test(request):
-    return render_to_response('test.html', {})
+    return render_to_response('query.html', {})
 
 def json(request):
-    #content = {
-    #    'id':'1',
-    #    'name':'test',
-    #}
-    #return JsonResponse(content)
-    print "Subzero POST data:", request.POST
-    print "Subzero GET data:", request.GET
-    return render_to_response('test.json', {})
+    content = {
+        'items':[
+            {
+                'name':'Alabama',
+                'label':'Alabama',
+                'abbreviation':'AL',
+            },
+            {
+                'name':'Alaska',
+                'label':'Alaska',
+                'abbreviation':'AK',
+            }
+        ]
+    }
+    return JsonResponse(content)
+    #print "Subzero POST data:", request.POST
+    #print "Subzero GET data:", request.GET
+
