a5d925f33a3a25adc8a461b056a542b742e0b9a2
subzero.debian <subzero.debian@localhost>
2007-10-25 14:38:30 +0000

    Query test fixed

--- apps/test/views.py
+++ apps/test/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from serpantin.apps.common.models import Town
 
 import simplejson
 
@@ -15,22 +16,36 @@ class JsonResponse(HttpResponse):
 def test(request):
     return render_to_response('query.html', {})
 
+def model_store(app_name, model_name, query = ""):
+    #FIXME: get rid of serpantin.apps in getattr
+    model = getattr(__import__('serpantin.apps.%s.models' % (app_name), '', '', [model_name]), model_name)
+    if len(query) and query[-1] == '*': query = query[:-1]
+    objects_filtered = model.objects.filter(name__istartswith=query)
+    return [{'name':i.name, 'label':i.name} for i in objects_filtered]
+    
 def json(request):
-    content = {
-        'items':[
-            {
-                'name':'Alabama',
-                'label':'Alabama',
-                'abbreviation':'AL',
-            },
-            {
-                'name':'Alaska',
-                'label':'Alaska',
-                'abbreviation':'AK',
-            }
-        ]
-    }
-    return JsonResponse(content)
-    #print "Subzero POST data:", request.POST
-    #print "Subzero GET data:", request.GET
-
+    content = [
+        {
+            'name':'Alabama',
+            'label':'Alabama',
+            'abbreviation':'AL',
+        },
+        {
+            'name':'Alaska',
+            'label':'Alaska',
+            'abbreviation':'AK',
+        },
+        {
+            'name':'American Samoa',
+            'label':'American Samoa',
+            'abbreviation':'AS',
+        },
+    ]
+    print "Subzero GET data:", request.GET
+    query = ""
+    if 'q' in request.GET: query = request.GET['q']
+    #if len(query) and query[-1] == '*': query = query[:-1]
+    #result = [i for i in content if not (query and i['name'].lower().find(query.lower()))]
+    result = model_store('common', 'Town', query)
+    
+    return JsonResponse({'items':result})
