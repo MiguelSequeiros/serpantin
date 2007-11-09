500469df3212fb5d618a5e397516f781d43fac51
artyom.opanchuk <artyom.opanchuk@localhost>
2007-11-09 17:10:16 +0000

    local settings

---
+++ local_settings-customize.py
@@ -0,0 +1,65 @@
+import os
+
+users = {
+    'ds': {
+        'login':	'ds',
+        'name':		'Dmitry Sorokin',
+        'email':	'dimas@dial.com.ru', 
+        'djangodir':	'/home/ds/svn/django/django', 
+        'projectdir':	'/home/ds/svn/serpantin', 
+    },
+    'greg': { 
+        'login':	'greg',
+        'name':		'Grigory Fateev',
+        'email':	'greg@dial.com.ru', 
+        'djangodir':	'/home/greg/www/django_src/django', 
+        'projectdir':	'/home/greg/www/serpantin', 
+    },
+    'subzero': { 
+        'login':	'subzero',
+        'name':		'Artem Opanchuk',
+        'email':	'subzero@dial.com.ru', 
+        'djangodir':	'/home/subzero/svn/django-trunk/django', 
+        'projectdir':	'/home/subzero/svn/serpantin', 
+    },
+}
+
+user = users[os.environ['USER']]
+
+DATABASE_ENGINE = 'postgresql' # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'serpantin'             # Or path to database file if using sqlite3.
+DATABASE_USER = os.environ['USER']             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. All choices can be found here:
+# http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+TIME_ZONE = 'Europe/Moscow'
+#TIME_ZONE = 'Europe/Moscow W-SU'
+#TIME_ZONE = 'Etc/GMT+3'
+
+# Language code for this installation. All choices can be found here:
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
+LANGUAGE_CODE = 'ru'
+
+DEFAULT_CHARSET = 'utf-8'
+
+SITE_ID = 1
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = '/var/www/html/media/'
+
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+MEDIA_URL = 'http://localhost/media'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'c6_+l+^%dw7@eqb&mzlc)xv18j9jsudl+=7_tm4z_76&8qvgd2'
