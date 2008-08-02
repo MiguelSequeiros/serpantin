595af57211e296c7c1972d3d89ec623e5ea9df70
dmitry.e.sorokin <dmitry.e.sorokin@localhost>
2008-08-02 22:09:20 +0000

    Fixes to make it work against django trunk

--- serpantin/apps/common/views.py
+++ serpantin/apps/common/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import ObjectPaginator
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext, Context, Template, TemplateDoesNotExist
 from django.template.loader import get_template
-from django.forms import FormWrapper
+#from django.forms import FormWrapper
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.db import models
